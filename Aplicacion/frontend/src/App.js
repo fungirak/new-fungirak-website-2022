226db3bd32837753ec01f7bcb44c1d272fe568a3
Gabriel Lazzarini <gabilazzarini88@outlook.com>
2022-02-02 19:02:35 -0300

    2do commit

--- Aplicacion/frontend/src/App.js
+++ Aplicacion/frontend/src/App.js
@@ -2,23 +2,14 @@ import React from 'react';
 import Portada from './componentes/Portada';
 import Barra from './componentes/Barra';
 
-
 const App = () => {
-    
-
-
     return (
       <div>
         <Portada />
         <Barra />
       </div>
-    )
-
-    
-    
+    )  
 }
-
-
 
 export default App
 
