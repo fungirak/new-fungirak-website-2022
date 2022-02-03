d3ca2488eff2452401f7d4f049f4ac58cbca3f25
Gabriel Lazzarini <gabilazzarini88@outlook.com>
2022-02-02 23:20:04 -0300

    otro commit mas

--- Aplicacion/frontend/src/componentes/helpers/IconPerfil.js
+++ Aplicacion/frontend/src/componentes/helpers/IconPerfil.js
@@ -4,12 +4,14 @@ import { Link } from "react-router-dom";
 const IconPerfil = ({pos, logo, ruta}) => {
   return <div>
       <>
-      <li className="nav efecto m-1 animate__animated animate__fadeInUp animate__delay-4s">
+      <li className="nav efecto m-1 animate__animated animate__fadeInUp animate__delay-1s">
+         
             <Link to={ruta}>
                 <button className="m-1 bg-transparent border border-0 efecto" onClick={pos} >
                     <img src={logo} className="icon-pres  p-2" alt="..." />
                 </button>
             </Link>
+        
       </li>
       </>
   </div>;
